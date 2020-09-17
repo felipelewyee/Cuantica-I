a9914bb6b35fff11fa75b1550702cd01bf52d1f9
Juan Felipe Huan Lew Yee <felipe.lew.yee@gmail.com>
2020-09-17 03:10:06 -0500

    Se reordenan celdas de MPn

--- _build/jupyter_execute/Moller-Plesset.py
+++ _build/jupyter_execute/Moller-Plesset.py
@@ -19,6 +19,8 @@ H 0.0000  0.0000 0.0000
 H 0.0000  0.0000 0.7414 
 ```
 
+# Defina molécula
+
 mol = psi4.geometry("""
 0 1
 H 0.0000  0.0000 0.0000
@@ -27,36 +29,34 @@ units angstrom
 symmetry C1
 """)
 
-# Defina molécula
-
 Ya que la teoría de Moller-Plesset corrige el Hamiltoniano de Hartree-Fock, **realice un cálculo de Hartree-Fock y recupere la energía y la función de onda**. La energía resultante contiene tanto energía electrónica como nuclear. Seleccione la base que desee, por ejemplo, 6-31G
+
+# Calcule E_HF y wfn
 
 E_HF, wfn = psi4.energy('HF/6-31G',return_wfn=True)
 print("E_HF =",E_HF)
 
-# Calcule E_HF y wfn
-
 **Obtenga la energía nuclear**
+
+# E_nuc
 
 E_nuc = mol.nuclear_repulsion_energy() 
 print("E_nuc =",E_nuc)
 
-# E_nuc
-
 **Obtenga los coeficientes de los orbitales moleculares a partir de la función de onda**
-
-C = np.asarray(wfn.Ca())
 
 # Coeficientes
 
+C = np.asarray(wfn.Ca())
+
 **Obtenga el número de funciones de base ($N_{bf}$), de orbitales moleculares ($N_{mo})$ y de electrones ($N_{e}$)**
+
+# nbf, nmo y ne
 
 nbf = len(C)
 nmo = wfn.nmo()
 ne = wfn.nalpha() + wfn.nbeta()
 print("nbf =",nbf," nmo =",nmo," ne =",ne)
-
-# nbf, nmo y ne
 
 Vamos a necesitar integrales de repulsión electónica
 
@@ -74,10 +74,10 @@ I_AO = np.asarray(mints.ao_eri())
 ~~~
 `````
 
+# ERIs I_AO
+
 mints = psi4.core.MintsHelper(wfn.basisset())
 I_AO = np.asarray(mints.ao_eri())
-
-# ERIs I_AO
 
 ```{Note}
 Los software de estructura electrónica usan formas optimizados de las ecuaciones aquí mostradas. Este notebook ha sido creado sólo con fines didácticos.
@@ -107,6 +107,8 @@ I_MO = np.einsum('mp,nq,sr,lt,mnsl->pqrt',C,C,C,C,I_AO,optimize=True)
 La cual es sustancialmente más rápida que los 8 for. Esto puede hacer su programa más rápido si su sistema no es tan pequeño.
 `````
 
+# ERIs I_MO
+
 I_MO = np.zeros((nmo,nmo,nmo,nmo))
 for p in range(nmo):
     for q in range(nmo):
@@ -118,8 +120,6 @@ for p in range(nmo):
                             for l in range(nbf):
                                 I_MO[p][q][r][t] = I_MO[p][q][r][t] + C[m][p]*C[n][q]*C[s][r]*C[l][t]*I_AO[m][n][s][l]
 
-# ERIs I_MO
-
 **Obtenga las energías de los orbitales moleculares ($\varepsilon_a$)**
 `````{tip}
 Puede usar el siguiente código
@@ -128,9 +128,9 @@ epsilon = wfn.epsilon_a()
 ~~~
 `````
 
-epsilon = np.array(wfn.epsilon_a())
-
 # epsilon
+
+epsilon = np.array(wfn.epsilon_a())
 
 Recordando teoría de perturbaciones, es posible realizar correcciones de n-orden a la energía $E_i^{(n)}$, tal que la energía electrónica total del estado basal ($i=0$) es
 
@@ -158,6 +158,8 @@ donde los índices $a$, $b$ refieren a orbitales moleculares ocupados, y los té
 Aunque las sumas empiezan en el primer orbital molecular ocupado, recuerde que en Python los índices empiezan en cero.
 ```
 
+# E_0 y E_1
+
 E_0 = 0
 for a in range(int(ne/2)):
     E_0 = E_0 + 2*epsilon[a]
@@ -167,18 +169,16 @@ for a in range(int(ne/2)):
     for b in range(int(ne/2)):
         E_1 = E_1 - 2 * I_MO[a][a][b][b] + I_MO[a][b][b][a]
 
-# E_0 y E_1
-
 **Calcule la energía total de MP1**, recuerde sumar la energía nuclear, es decir
 
 $$
 E_{MP1} = E_{nuc} + E_0^{(0)} + E_0^{(1)}
 $$
 
+# E_MP1
+
 E_MP1 = E_nuc + E_0 + E_1
 print("E_MP1 =",E_MP1)
-
-# E_MP1
 
 Calcule la diferencia entre $E_{MP1}$ y la energía de Hartree-Fock que calculó al inicio, **¿Cuál es la energía de correlación? ¿Cómo se relaciona $E_{MP1}$ con $E_{HF}$?**
 
@@ -210,6 +210,8 @@ $$
 E_{corr} = E_{MP2} - E_{HF}
 $$
 
+# E_2, E_MP2 y E_corr
+
 E_2 = 0
 for a in range(int(ne/2)):
     for b in range(int(ne/2)):
@@ -222,8 +224,6 @@ E_MP2 = E_nuc + E_0 + E_1 + E_2
 print("E_MP2 =",E_MP2)
 
 print("E_corr =",E_MP2-E_HF)
-
-# E_2, E_MP2 y E_corr
 
 ```{important}
 Generalizando, la energía total de MPn es
@@ -245,7 +245,7 @@ E_MPn = psi4.energy('MPn/Base')
 print("E_MPn =",E_MPn)
 ```
 
+# E_MP2 psi4
+
 E_MP2 = psi4.energy('MP2/6-31G')
 print("E_MP2 =",E_MP2)
-
-# E_MP2 psi4
