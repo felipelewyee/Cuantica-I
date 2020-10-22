178e6732f677658242a0933fdcf2118d4a7224e0
Juan Felipe Huan Lew Yee <felipe.lew.yee@gmail.com>
2020-10-22 15:09:01 -0500

    Se agregan notebooks faltantes

---
+++ _build/jupyter_execute/Esfera.py
@@ -0,0 +1,116 @@
+# Partícula en la esfera
+
+Se tiene una partícula moviéndose sobre una superficie esférica de radio constante.
+
+La ecuación de Schrödinger a resolver es
+
+$$
+-\frac{\hbar^2}{2m} \nabla^2 \psi(\theta,\phi) = E \psi(\theta,\phi)
+$$
+
+```{admonition} Inserto matemático: Hamiltoniano
+:class: dropdown
+Donde
+$$
+\nabla^2 = \frac{1}{r} \frac{\partial^2}{\partial r^2}r + \frac{1}{r^2} \Lambda^2 = \frac{1}{r} \frac{\partial^2}{\partial r^2} r + \frac{1}{r^2} \left( \frac{1}{sin^2 \theta} \frac{\partial^2}{\partial \phi^2} + \frac{1}{sin \theta} \frac{\partial}{\partial \theta} sin \theta \frac{\partial}{\partial \theta} \right)
+$$
+
+Si r es constante, entonces
+$$
+-\frac{\hbar^2}{2mr^2} \Lambda^2 \psi(\theta,\phi) = E \psi(\theta,\phi)
+$$
+```
+
+Las soluciones de esta ecuación son los armónicos esféricos.
+
+```{admonition} Inserto matemático: Armónicos esféricos
+:class: dropdown
+
+Los armónicos esféricos se definen por
+
+$$
+Y_l^{m_l}(\theta,\phi) = \sqrt{\frac{2l+1}{4\pi} \frac{(l-|m_l|)!}{(l+|m_l|)!}} e^{i m_l \phi} P_l^{m_l}(cos(\theta))
+$$
+
+donde $P_l^{m_l}(cos(\phi))$ son los polinomios asociados de Legendre, dados por
+$$
+P_l^{m_l}(x) = \frac{l}{2^l l!}(1-x^2)^{|m_l|/2} \frac{d^{l+|m_l|}}{dx^{l+|m_l|}} (x^2-1)^l
+$$
+```
+
+En la tabla se muestra la forma de los primeros armónicos esféricos. En este punto han aparecido dos números cuánticos, tal que $l = 0,1,2,3,...$ y $m_l = -l, -l+1, 0, l-1, l$
+
+
+|$l$|$m_l$|Armónico esférico $Y_l^{m_l}(\theta,\phi)$|
+|---|---|---|
+|0|0|$\frac{1}{(4\pi)^{1/2}}$|
+|1|-1|$+\frac{3}{(8\pi)^{1/2}} sin \theta e^{-i\phi}$|
+|1|0|$\frac{3}{(4\pi)^{1/2}} cos \theta$|
+|1|1|$-\frac{3}{(8\pi)^{1/2}} sin \theta e^{i\phi}$|
+|2|-2|$+\frac{15}{(32\pi)^{1/2}} sin^2 \theta e^{-2i\phi}$|
+|2|-1|$+\frac{15}{(8\pi)^{1/2}} sin \theta cos \theta e^{-i\phi}$|
+|2|0|$\frac{5}{(16\pi)^{1/2}} (3cos^2 \theta - 1)$|
+|2|1|$-\frac{15}{(8\pi)^{1/2}} sin \theta cos \theta e^{i\phi}$|
+|2|2|$-\frac{15}{(32\pi)^{1/2}} sin^2 \theta e^{2i\phi}$|
+
+La energía del sistema está dada por
+
+$$
+E = -l(l+1)
+$$
+
+**Grafique el armónico esférico $|Y_1^{0}|$ y su cuadrado.**
+
+# Grafica
+
+#Grafica
+
+import scipy.special as sp
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+
+ml=0
+l=1
+
+theta = np.linspace(0,np.pi,100)
+phi = np.linspace(0,2*np.pi,100)
+THETA,PHI=np.meshgrid(theta,phi)
+
+R=np.abs(sp.sph_harm(ml,l,PHI,THETA))
+
+X = R * np.sin(THETA) * np.cos(PHI)
+Y = R * np.sin(THETA) * np.sin(PHI)
+Z = R * np.cos(THETA)
+
+fig = plt.figure()
+ax = plt.gca(projection='3d')
+ax.plot_surface(X, Y, Z,cmap='YlOrRd')
+#ax.set_xlim(-0.4,0.4)
+#ax.set_ylim(-0.4,0.4)
+#ax.set_zlim(-0.4,0.4)
+ax.set_title("$\Psi$")
+plt.show()
+
+R=np.power(R,2.0)
+
+X = R * np.sin(THETA) * np.cos(PHI)
+Y = R * np.sin(THETA) * np.sin(PHI)
+Z = R * np.cos(THETA)
+
+fig = plt.figure()
+ax = plt.gca(projection='3d')
+ax.plot_surface(X, Y, Z,cmap='YlOrRd')
+ax.set_xlim(-0.2,0.2)
+ax.set_ylim(-0.2,0.2)
+ax.set_zlim(-0.2,0.2)
+ax.set_title("$\Psi^2$")
+plt.show()
+
+## Referencias
+
+- Atkins, P. W.; Friedman, R. Molecular Quantum Mechanics, 4th ed.; Oxford University Press: New York, 2005.
+- Zettili, N. Quantum Mechanics: Concepts and Applications, 2nd ed.; Wiley: Chichester, U.K, 2009.
+- Levine, I. N. Quantum Chemistry, 5th ed.; Prentice Hall: Upper Saddle River, N.J, 2000.
+- McQuarrie, D. A.; Simon, J. D. Physical Chemistry: A Molecular Approach; University Science Books: Sausalito, Calif, 1997.
---
+++ _build/jupyter_execute/Rotor.py
@@ -0,0 +1,100 @@
+# Rotor Rígido
+
+Este sistema consiste en dos partículas de masa $m_1$ y $m_2$ moviéndose con una separación constante $r=r_2-r_1$.
+
+El Hamiltoniano consta de los términos de energía cinética de ambas partículas
+
+$$
+H = -\frac{\hbar^2}{2m_1} \nabla^2_1 -\frac{\hbar^2}{2m_2} \nabla^2_2
+$$
+
+```{admonition} Inserto matemático: Sistema de masa reducida
+:class: dropdown
+
+Este sistema es equivalente al de una partícula de masa reducida girando en torno al centro de masa de ambas partículas. La masa total del sistema está dada por la suma de las masas de las partículas
+
+$$
+m_T = m_1 + m_2
+$$
+
+La masa reducida de la nueva partícula está dada por
+
+$$
+\frac{1}{\mu} = \frac{1}{m_1} + \frac{1}{m_2}
+$$
+
+El centro de masa del sistema se calcula mediante
+
+$$
+R_{cm} = \left( \frac{m_1}{m_T} \right) r_1 + \left( \frac{m_2}{m_T} \right) r_2
+$$
+
+y el Hamiltoniano se calcula con la energía cinética de la masa reducida y del centro de masa, es decir
+
+$$
+H = -\frac{\hbar^2}{2m_T} \nabla^2_{R_{cm}} - \frac{\hbar^2}{2\mu} \nabla^2_{r}
+$$
+
+La ecuación de Schrödinger a resolver es
+
+$$
+\left(-\frac{\hbar^2}{2m_T} \nabla^2_{R_{cm}} - \frac{\hbar^2}{2\mu} \nabla^2_{r}\right) \psi = E \psi
+$$
+
+Se propone que la función de onda se puede separar en el producto de una función de onda del centro de masa y una función de onda de la partícula de masa reducida
+
+$$
+\psi=\psi_{cm}\psi_r
+$$
+
+Al sustituir en la ecuación de Schrödinger se obtiene
+
+$$
+\left(-\frac{\hbar^2}{2m_T} \nabla^2_{R_{cm}} - \frac{\hbar^2}{2\mu} \nabla^2_{r}\right) \psi_{cm}\psi_r = E \psi_{cm}\psi_r
+$$
+
+Si consideramos que la energía está dada por $E_T = E_{cm} + E_{r}$ y distribuimos, resulta
+
+$$
+-\psi_{r} \frac{\hbar^2}{2m_T} \nabla^2_{R_{cm}} \psi_{cm} - \psi_{cm}\frac{\hbar^2}{2\mu} \nabla^2_{r} \psi_r = \psi_r E_{cm} \psi_{cm} +  \psi_{cm} E_r \psi_r
+$$
+
+Si multiplicamos ambos lados de la ecuación anterior por $\frac{1}{\psi_{r}\psi_{cm}}$, resulta
+
+$$
+-\frac{1}{\psi_{cm}} \left( \frac{\hbar^2}{2m_T} \nabla^2_{R_{cm}} \psi_{cm} - E_{cm} \psi_{cm} \right) = \frac{1}{\psi_{r}} \left( \frac{\hbar^2}{2\mu} \nabla^2_{r} \psi_r + E_r \psi_r \right)
+$$
+
+ya que el lado izquierdo solo depende de las coordenadas del centro de masa, y el lado derecho solo depende de las coordenadas de la masa reducida, ambos lados deben ser igual a una constante. Si elegimos esta constante como cero, y despejamos lo que está dentro de cada paréntesis se obtienen dos ecuaciones independientes.
+```
+
+Después de cambiar a un sistema de masa reducida se obtienen dos ecuaciones.
+
+La primera ecuación corresponde al movimiento del `centro de masa del sistema` y la hemos estudiado previamente en el movimiento de la `partícula libre`
+
+$$
+-\frac{\hbar^2}{2m_T} \nabla^2_{R_{cm}} \psi_{cm} = E_{cm} \psi_{cm}
+$$
+
+esta ecuación tiene como soluciones
+
+$$
+\psi_{cm} = A e^{ikx} + B e^{-ikx}
+$$
+
+con $k^2=2m_TE/\hbar^2$, y simplemente nos dice que el sistema en conjunto se mueve libremente por el espacio.
+
+La segunda ecuación corresponde a la `masa reducida`, y la hemos estudiado en la `partícula en la esfera`
+
+$$
+-\frac{\hbar^2}{2\mu} \nabla^2_{r} \psi_r = E_r \psi_r
+$$
+
+sabemos por tanto que su solución son los armónico esféricos $Y_l^{m_l}(\theta,\phi)$ con $E = -l(l+1)$.
+
+## Referencias
+
+- Atkins, P. W.; Friedman, R. Molecular Quantum Mechanics, 4th ed.; Oxford University Press: New York, 2005.
+- Zettili, N. Quantum Mechanics: Concepts and Applications, 2nd ed.; Wiley: Chichester, U.K, 2009.
+- Levine, I. N. Quantum Chemistry, 5th ed.; Prentice Hall: Upper Saddle River, N.J, 2000.
+- McQuarrie, D. A.; Simon, J. D. Physical Chemistry: A Molecular Approach; University Science Books: Sausalito, Calif, 1997.
